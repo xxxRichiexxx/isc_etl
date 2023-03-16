64b2d5c9912ffa1660b2e30cf2a511254aad2ab6
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-03-16 21:22:28 +0300

    v0

--- isc_dag.py
+++ isc_dag.py
@@ -39,8 +39,8 @@ with DAG(
         'isc',
         default_args=default_args,
         description='Получение данных из ИСК.',
-        start_date=dt.datetime(2023, 2, 27),
-        schedule_interval='@daily',
+        start_date=dt.datetime(2021, 1, 1),
+        schedule_interval='@monthly',
         catchup=True,
         max_active_runs=1
 ) as dag:
