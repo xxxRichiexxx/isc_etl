dbd212e51f23882d6f86dccfbc8000462b00b9de
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-03-16 19:39:06 +0300

    v0

--- scripts/dm_isc_sales_t_check.sql
+++ scripts/dm_isc_sales_t_check.sql
@@ -1,11 +1,19 @@
 INSERT INTO sttgaz.stage_checks (table_name, check_name, ts, check_result)
+WITH
+    sq AS(
+        SELECT
+            SUM('Продажи в розницу') 
+            -- AND SUM('Продажи физ лицам') = {{task_instance.xcom_pull(key='SoldToIndividuals', task_ids='Загрузка_данных_в_stage_слой.get_sales')}}
+            -- AND SUM('Остатки на НП') = {{task_instance.xcom_pull(key='BalanceAtBeginningOfPeriod', task_ids='Загрузка_данных_в_stage_слой.get_sales')}}
+            -- AND SUM('Остатки на КП') = {{task_instance.xcom_pull(key='BalanceAtEndOfPeriod', task_ids='Загрузка_данных_в_stage_слой.get_sales')}}
+        FROM sttgaz.dm_isc_sales_t
+        WHERE "Период" = '{{execution_date.date().replace(day=1)}}';
+    )
 SELECT
 	'dm_isc_sales_t_check',
 	'checking_for_accuracy_of_execution',
 	NOW(),
-    SUM('Продажи в розницу') = {{task_instance.xcom_pull(key='SoldAtRetail', task_ids='Загрузка_данных_в_stage_слой.get_sales')}}
+    (select * from sq) = {{task_instance.xcom_pull(key='SoldAtRetail', task_ids='Загрузка_данных_в_stage_слой.get_sales')}}
     -- AND SUM('Продажи физ лицам') = {{task_instance.xcom_pull(key='SoldToIndividuals', task_ids='Загрузка_данных_в_stage_слой.get_sales')}}
     -- AND SUM('Остатки на НП') = {{task_instance.xcom_pull(key='BalanceAtBeginningOfPeriod', task_ids='Загрузка_данных_в_stage_слой.get_sales')}}
     -- AND SUM('Остатки на КП') = {{task_instance.xcom_pull(key='BalanceAtEndOfPeriod', task_ids='Загрузка_данных_в_stage_слой.get_sales')}}
-FROM sttgaz.dm_isc_sales_t
-WHERE "Период" = '{{execution_date.date().replace(day=1)}}';
