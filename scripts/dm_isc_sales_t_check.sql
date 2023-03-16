e256dfd0e4a4cc18fee72809dbfe032867813ad6
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-03-16 19:27:24 +0300

    v0

--- scripts/dm_isc_sales_t_check.sql
+++ scripts/dm_isc_sales_t_check.sql
@@ -3,7 +3,7 @@ SELECT
 	'dm_isc_sales_t_check',
 	'checking_for_accuracy_of_execution',
 	NOW(),
-    SUM('Продажи в розницу') = {{task_instance.pull(key='SoldAtRetail', task_ids='get_sales')}}
+    SUM('Продажи в розницу') = {{task_instance.xcom_pull(key='SoldAtRetail', task_ids='get_sales')}}
     AND SUM('Продажи физ лицам') = {{task_instance.xcom_pull(key='SoldToIndividuals', task_ids='get_sales')}}
     AND SUM('Остатки на НП') = {{task_instance.xcom_pull(key='BalanceAtBeginningOfPeriod', task_ids='get_sales')}}
     AND SUM('Остатки на КП') = {{task_instance.xcom_pull(key='BalanceAtEndOfPeriod', task_ids='get_sales')}}
