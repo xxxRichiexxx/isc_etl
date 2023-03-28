7518810bd6e198c8ec06661dce1464563315c8d2
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-03-28 18:28:12 +0300

    v3

--- scripts/dm_isc_sales_tv_check.sql
+++ scripts/dm_isc_sales_tv_check.sql
@@ -2,10 +2,12 @@ INSERT INTO sttgaz.stage_checks (table_name, check_name, ts, check_result)
 SELECT
 	'{{params.dm}}',
 	SUM("Продажи в розницу") || '; {{task_instance.xcom_pull(key='SoldAtRetail', task_ids='Загрузка_данных_в_stage_слой.get_sales')}} ||' ||
+    SUM("Продажи физ лицам") || '; {{task_instance.xcom_pull(key='SoldToIndividuals', task_ids='Загрузка_данных_в_stage_слой.get_sales')}} ||' ||
     SUM("Остатки на НП") || '; {{task_instance.xcom_pull(key='BalanceAtBeginningOfPeriodOnRoad', task_ids='Загрузка_данных_в_stage_слой.get_sales')}} ||' ||
     SUM("Остатки на КП") || '; {{task_instance.xcom_pull(key='BalanceAtEndOfPeriodOnRoad', task_ids='Загрузка_данных_в_stage_слой.get_sales')}}',
 	'{{execution_date.date()}}',
     SUM("Продажи в розницу") = {{task_instance.xcom_pull(key='SoldAtRetail', task_ids='Загрузка_данных_в_stage_слой.get_sales')}}
+    AND SUM("Продажи физ лицам") = {{task_instance.xcom_pull(key='SoldToIndividuals', task_ids='Загрузка_данных_в_stage_слой.get_sales')}}
     AND SUM("Остатки на НП") = {{task_instance.xcom_pull(key='BalanceAtBeginningOfPeriodOnRoad', task_ids='Загрузка_данных_в_stage_слой.get_sales')}}
     AND SUM("Остатки на КП") = {{task_instance.xcom_pull(key='BalanceAtEndOfPeriodOnRoad', task_ids='Загрузка_данных_в_stage_слой.get_sales')}}
 FROM sttgaz.{{params.dm}}
