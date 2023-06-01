e1587557a21d3c8a2fa21073687513ee3b8461f4
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-06-01 12:26:07 +0300

    v9

--- isc_dag_realization.py
+++ isc_dag_realization.py
@@ -40,7 +40,7 @@ with DAG(
         default_args=default_args,
         description='Получение данных из ИСК. Реализация.',
         start_date=dt.datetime(2018, 1, 1),
-        schedule_interval='@monthly',
+        schedule_interval='@daily',
         catchup=True,
         max_active_runs=1
 ) as dag:
