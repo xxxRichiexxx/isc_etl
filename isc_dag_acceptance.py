36756a06ebe6c5f2b3dffc19aa485a7748a871e4
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-06-27 09:52:54 +0300

    v30

---
+++ isc_dag_acceptance.py
@@ -0,0 +1,173 @@
+
+import sqlalchemy as sa
+from urllib.parse import quote
+import datetime as dt
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.task_group import TaskGroup
+from airflow.hooks.base import BaseHook
+from airflow.operators.dummy import DummyOperator
+from airflow.contrib.operators.vertica_operator import VerticaOperator
+from airflow.operators.python import BranchPythonOperator
+
+from isc_etl.scripts.collable import etl, date_check
+
+
+source_con = BaseHook.get_connection('isc')
+source_username = source_con.login
+source_password = quote(source_con.password)
+source_host = source_con.host
+source_db = source_con.schema
+eng_str = fr'mssql://{source_username}:{source_password}@{source_host}/{source_db}?driver=ODBC Driver 18 for SQL Server&TrustServerCertificate=yes'
+source_engine = sa.create_engine(eng_str)
+
+dwh_con = BaseHook.get_connection('vertica')
+ps = quote(dwh_con.password)
+dwh_engine = sa.create_engine(
+    f'vertica+vertica_python://{dwh_con.login}:{ps}@{dwh_con.host}:{dwh_con.port}/sttgaz'
+)
+
+
+default_args = {
+    'owner': 'Швейников Андрей',
+    'email': ['xxxRichiexxx@yandex.ru'],
+    'retries': 3,
+    'retry_delay': dt.timedelta(minutes=30),
+}
+with DAG(
+        'isc_acceptance',
+        default_args=default_args,
+        description='Получение данных из ИСК. Реализация.',
+        start_date=dt.datetime(2018, 1, 1),
+        schedule_interval='@monthly',
+        catchup=True,
+        max_active_runs=1
+) as dag:
+
+    data_types = [
+            'acceptance',
+        ]
+
+    start = DummyOperator(task_id='Начало')
+
+    with TaskGroup('Загрузка_данных_в_stage_слой') as data_to_stage:
+
+        daily_tasks = []
+
+        for data_type in data_types:
+            daily_tasks.append(
+                PythonOperator(
+                    task_id=f'get_{data_type}',
+                    python_callable=etl,
+                    op_kwargs={
+                        'data_type': data_type,
+                        'source_engine': source_engine,
+                        'dwh_engine': dwh_engine,
+                    },
+                )
+            )
+
+        date_check = BranchPythonOperator(
+            task_id='date_check',
+            python_callable=date_check,
+            op_kwargs={
+                'taskgroup': 'Загрузка_данных_в_stage_слой',
+                },
+        )
+
+        do_nothing = DummyOperator(task_id='do_nothing')
+
+        monthly_tasks = PythonOperator(
+                    task_id='monthly_tasks',
+                    python_callable=etl,
+                    op_kwargs={
+                        'data_type': 'acceptance',
+                        'source_engine': source_engine,
+                        'dwh_engine': dwh_engine,
+                        'monthly_tasks': True,
+                    },
+                )
+        
+        collapse = DummyOperator(
+            task_id='collapse',
+            trigger_rule='none_failed',
+        )
+
+        daily_tasks >> date_check >> [do_nothing, monthly_tasks] >> collapse
+
+    with TaskGroup('Загрузка_данных_в_dds_слой') as data_to_dds:
+
+        pass
+
+        # data_types = [
+        #     'DirectionOfImplementationWithUKP',
+        #     'counteragent',
+        #     'manufacturer',
+        #     'division',
+        #     'dealer_unit',
+        # ]
+
+        # tasks = []
+
+        # for data_type in data_types:
+        #     tasks.append(
+        #         VerticaOperator(
+        #             task_id=f'dds_isc_{data_type}',
+        #             vertica_conn_id='vertica',
+        #             sql=f'scripts/dds_isc_{data_type}.sql',
+        #             params={
+        #                 'delta_1': dt.timedelta(days=1),
+        #                 'delta_2': dt.timedelta(days=4),
+        #             }
+        #         )
+        #     )
+
+        # product = VerticaOperator(
+        #     task_id=f'dds_isc_product',
+        #     vertica_conn_id='vertica',
+        #     sql=f'scripts/dds_isc_product.sql',
+        #     params={
+        #         'delta_1': dt.timedelta(days=1),
+        #         'delta_2': dt.timedelta(days=4),
+        #     }
+        # )
+
+        # realization = VerticaOperator(
+        #     task_id=f'dds_isc_realization',
+        #     vertica_conn_id='vertica',
+        #     sql=f'scripts/dds_isc_realization.sql',
+        #     params={
+        #         'delta_1': dt.timedelta(days=1),
+        #         'delta_2': dt.timedelta(days=4),
+        #     }
+        # )
+
+        # tasks >> product >> realization
+
+    with TaskGroup('Загрузка_данных_в_dm_слой') as data_to_dm:
+
+        pass
+
+        # dm_isc_realization_v = VerticaOperator(
+        #             task_id='dm_isc_realization_v',
+        #             vertica_conn_id='vertica',
+        #             sql='scripts/dm_isc_realization_v.sql',
+        #         )
+        
+    with TaskGroup('Проверки') as data_checks:
+
+        pass
+
+        # dm_isc_realization_v_check = VerticaOperator(
+        #             task_id='dm_isc_realization_v_check',
+        #             vertica_conn_id='vertica',
+        #             sql='scripts/dm_isc_realization_v_check.sql',
+        #             params={
+        #                 'dm': 'dm_isc_realization_v',
+        #             }
+        #         )
+
+    end = DummyOperator(task_id='Конец')
+
+    start >> data_to_stage >> data_to_dds >> data_to_dm >> data_checks >> end
--- scripts/collable.py
+++ scripts/collable.py
@@ -6,13 +6,15 @@ def extract(source_engine, data_type, execution_date):
     """Извлечение данных из источника."""
 
     print('ИЗВЛЕЧЕНИЕ ДАННЫХ')
+
+    date_from = execution_date.replace(day=1)
+    date_to = (execution_date.replace(day=28) + dt.timedelta(days=4)) \
+        .replace(day=1) - dt.timedelta(days=1)
+    
     with open(
         fr'/home/da/airflow/dags/isc_etl/scripts/stg_{data_type}.sql', 'r'
     ) as f:
-        date_from = execution_date.replace(day=1)
-        date_to = (execution_date.replace(day=28) + dt.timedelta(days=4)) \
-            .replace(day=1) - dt.timedelta(days=1)
-        command = f.read().format(date_from, date_to)
+        command = f.read().format(date_from, date_to) if data_type in ('sales', 'realization') else f.read().format(date_from)
 
     print(command)
 
