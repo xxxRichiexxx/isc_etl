8161b0482eba96a6bd7379df5df45274c06c88ea
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-10-17 09:22:44 +0300

    add field to orders

--- scripts/dm_isc_contracting_check.sql
+++ scripts/dm_isc_contracting_check.sql
@@ -1,9 +1,9 @@
 INSERT INTO sttgaz.stage_checks (table_name, check_name, ts, check_result)
 SELECT
 	'{{params.dm}}',
-	SUM("Догруз на начало месяца") || '; 715',
+	'Догруз на начало месяца: ' || SUM("Догруз на начало месяца") || ', 715; ' ||  'Догруз на конец месяца: ' || SUM("Догруз на конец месяца") || ', 1236;', 
 	'{{execution_date.date()}}',
-    SUM("Догруз на начало месяца") = 715
+    SUM("Догруз на начало месяца") = 715 AND SUM("Догруз на конец месяца") = 1236
 FROM sttgaz.{{params.dm}}
 WHERE DATE_TRUNC('MONTH', "Период") = '2023-08-01'
 	AND "Направление реализации" LIKE 'РФ%'
